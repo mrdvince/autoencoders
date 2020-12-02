dd7757799258661ecc777a1111473150a78df099
Droid021 <vincivenv@gmail.com>
2020-12-02 10:27:53 +0300

    Train loop

--- conv-autoencoder.py
+++ conv-autoencoder.py
@@ -73,3 +73,70 @@ class ConvAutoencoder(nn.Module):
 
 model = ConvAutoencoder()
 print(model)
+
+# specify loss function
+criterion = nn.MSELoss()
+
+# specify loss function
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+# number of epochs to train the model
+n_epochs = 30
+
+for epoch in range(1, n_epochs+1):
+    # monitor training loss
+    train_loss = 0.0
+
+    ###################
+    # train the model #
+    ###################
+    for data in train_loader:
+        # _ stands in for labels, here
+        # no need to flatten images
+        images, _ = data
+        # clear the gradients of all optimized variables
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs
+        # by passing inputs to the model
+        outputs = model(images)
+        # calculate the loss
+        loss = criterion(outputs, images)
+        # backward pass: compute
+        # gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        # update running training loss
+        train_loss += loss.item()*images.size(0)
+
+    # print avg training statistics
+    train_loss = train_loss/len(train_loader)
+    print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+        epoch,
+        train_loss
+    ))
+
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
+
+# get sample outputs
+output = model(images)
+# prep images for display
+images = images.numpy()
+
+# output is resized into a batch of iages
+output = output.view(batch_size, 1, 28, 28)
+# use detach when it's an output that requires_grad
+output = output.detach().numpy()
+
+# plot the first ten input images and then reconstructed images
+fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True,
+                         sharey=True, figsize=(25, 4))
+
+# input images on top row, reconstructions on bottom
+for images, row in zip([images, output], axes):
+    for img, ax in zip(images, row):
+        ax.imshow(np.squeeze(img), cmap='gray')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
