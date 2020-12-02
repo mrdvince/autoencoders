0df64d28472ffbfa6838fc0468e34951c86345bf
Droid021 <vincivenv@gmail.com>
2020-12-02 09:31:28 +0300

    fix: Image shape

--- simple-autoencoder.py
+++ simple-autoencoder.py
@@ -78,15 +78,21 @@ for epoch in range(1, epochs+1):
 dataiter = iter(test_loader)
 images, labels = dataiter.next()
 
-img_flatten = images.view(images.size(0), -1)
-output = model(img_flatten)
+images_flatten = images.view(images.size(0), -1)
+# get sample outputs
+output = model(images_flatten)
+# prep images for display
 images = images.numpy()
 
+# output is resized into a batch of images
+output = output.view(batch_size, 1, 28, 28)
+# use detach when it's an output that requires_grad
 output = output.detach().numpy()
 
-# Plot [0] input images and the reconstructed images
 fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True,
                          sharey=True, figsize=(25, 4))
+
+# input images on top row, reconstructions on bottom
 for images, row in zip([images, output], axes):
     for img, ax in zip(images, row):
         ax.imshow(np.squeeze(img), cmap='gray')
